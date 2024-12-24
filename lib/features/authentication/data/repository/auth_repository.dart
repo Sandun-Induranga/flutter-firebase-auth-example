4323187e56561de16d6c3f954c1da5288177ada8
Sandun Induranga <sandutharu40@gmail.com>
2024-12-24 14:43:33 +0530

    Content Updated

--- lib/features/authentication/data/repository/auth_repository.dart
+++ lib/features/authentication/data/repository/auth_repository.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class Auth {
+class AuthRepository {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   User? get currentUser => _firebaseAuth.currentUser;
